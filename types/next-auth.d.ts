502e43483df7b30137788418a4bf78cf3e90c377
RyryTheGuy <ryanroemer61@gmail.com>
2022-05-01 13:27:25 -0500

    Defined User

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -5,11 +5,17 @@ declare module "next-auth" {
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
-    user: {
-      id: string,
-      oauth?: boolean,
-      bio?: string,
-      phone?: string,
-    } & DefaultSession['user']
+    user: User
+  }
+
+  interface User {
+    id: string,
+    email: string,
+    oauth: boolean,
+    image?: string,
+    bio?: string,
+    phone?: string,
+    name?: string,
+    password?: string,
   }
 }
